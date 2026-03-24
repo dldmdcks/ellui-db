9a85688adc5d9f004dc62c49db63cf0a1c9c0795
dldmdcks <dldmdcks94@gmail.com>
2026-03-24 13:56:44 +0900

    Update app.py

--- app.py
+++ app.py
@@ -82,9 +82,9 @@ try:
 
     # 현재 서버가 렌더(Render)인지 확인하고 알아서 귀가 주소 변경하기
     if os.getenv("RENDER"):
-        auto_redirect_uri = "https://ellui-db.onrender.com/"
+        REDIRECT_URI = "https://ellui-db.onrender.com/"
     else:
-        auto_redirect_uri = "https://ellui-db.streamlit.app/"
+        REDIRECT_URI = "https://ellui-db.streamlit.app/"
 
 except Exception:
     st.error("❌ 금고 설정(Secrets)을 확인해주세요.")
